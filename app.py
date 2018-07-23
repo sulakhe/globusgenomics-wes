4db7a9bfcfc7187800bc14d34e5c72a64a2e7272
Mark Xiao <mustxyk@gmail.com>
2018-07-22 22:19:58 -0500

    token code, get user name, tokens, add config file

--- app.py
+++ app.py
@@ -6,10 +6,19 @@ import weshandler
 import time
 import os
 import sys
+import globus_sdk
+import ConfigParser
 
 app = Flask(__name__)
-master_key = __config__("master_key")
-url = __config__("url")
+
+local_configs_dir = '/home/galaxy/.globusgenomics'
+config_file_name = 'commons_app_config'
+config_file = os.path.join(local_configs_dir, config_file_name)
+Config = ConfigParser.ConfigParser()
+Config.read(config_file)
+
+master_key = Config.get("server", "master_key")
+url = Config.get("server", "url")
 cwl_runner_galaxy_workflow_minid = "ark:/57799/b93q6h"
 QUERY_BASE = "http://minid.bd2k.org/minid/landingpage/"
 
@@ -83,9 +92,50 @@ def __get_galaxy_user(auth):
     return api_key
 
 
-def __get_globus_user(auth):
-	# Using Auth token, get the globus username and return the username
-    return "sulakhe"
+def __get_globus_user(token):
+    # Using Auth token, get the globus username and return the username
+    # start globus client
+    client_id = Config.get('globus', 'client_id')
+    client_secret = Config.get('globus', 'client_secret')
+    redirect_uri = Config.get('globus', 'redirect_uri')
+    client = globus_sdk.ConfidentialAppAuthClient(client_id, client_secret)
+    client.oauth2_start_flow(redirect_uri)
+
+    # check whether token is active
+    check_result = client.oauth2_validate_token(token)
+    if not check_result['active']:
+        # ???? how how handle this
+        sys.exit('Token not active.')
+
+    # get username
+    token_info = client.oauth2_token_introspect(token)
+    username = token_info['username']
+    if '@' in username:
+        username = username[0:username.find('@')]
+
+    # get and record auth token and transfer token
+    dependent_token_info = client.oauth2_get_dependent_tokens(token)
+    transfer_data = dependent_token_info.by_resource_server['transfer.api.globus.org']
+    transfer_token = transfer_data['access_token']
+    auth_data = dependent_token_info.by_resource_server['auth.globus.org']
+    auth_token = auth_data['access_token']
+
+    dir_name =  os.path.join(local_configs_dir, 'tokens')
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name, 0700)
+    record_file = os.path.join(dir_name, username)
+    with open(record_file, 'w') as write_file:
+        write_file.write(transfer_token)
+
+    dir_name =  os.path.join(local_configs_dir, 'tokens-auth')
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name, 0700)
+    record_file = os.path.join(dir_name, username)
+    with open(record_file, 'w') as write_file:
+        write_file.write(auth_token)
+
+    return username
+    
 
 def __create_galaxy_user(globus_user, gi):
     user_email = "%s@globusid.org" % globus_user
